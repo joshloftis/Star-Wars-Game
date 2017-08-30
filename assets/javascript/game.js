4784b766c0f8b1e3f67ace45294d23a65a8db1c9
Josh Loftis <jshlfts32@gmail.com>
2017-08-30 19:43:36 -0400

    Drying up logic a bit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-  //var to hold characters, attack power, hp, and counter attack power
+  //object to hold characters, attack power, hp, and counter attack power
   var charAttr = {
+    image: ['src="assets/images/Luke_Skywalker.png"', 'src="assets/images/DarthVader3.png"', 'src="assets/images/rey.png"', 'src="assets/images/kyloren.png"' ],
     name: ["Luke", "Vader", "Rey", "Ren"],
     attack: [8, 10, 5, 4],
     health: [120, 100, 150, 180],
@@ -12,29 +13,19 @@ $(document).ready(function() {
   var chosenChar;
   var chosenEnemy;
 
-  //iterates through each .character image and applies the corresponding charAttr key and value
-  $('.character').each(function(i) {
-    $(this).data({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i]});
-  });
-
+  //iterates through each .charDiv and applies the corresponding charAttr key and value and then appends the characters name, image and health
   $('.charDiv').each(function(i) {
-    $(this).prepend('<div class="name">' + charAttr.name[i] + "</div>");
-    $(this).append('<div class="health">' + charAttr.health[i] + "</div>");
+    $(this).attr({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i]})
+    .append('<div class="name">' + charAttr.name[i] + "</div>")
+    .append('<img class="img-responsive center-block" ' + charAttr.image[i] + '>')
+    .append('<div class="health">' + charAttr.health[i] + "</div>");
   });
 
-    console.log($('#char1').data("name"));
-    console.log($('#char1').data("attack"));
-    console.log($('#char1').data("health"));
-    console.log($('#char1').data("counter"));
+  console.log($('#char1').attr("name"));
 
-  $('.character').on("click", function() {
-    //$("#playerArea").html(this);
+  $('.charDiv').on('click', function () {
+    console.log($(this).attr("name"));
+    
     playerChosen = true;
   });
-
-
-
-
-
-
 });
