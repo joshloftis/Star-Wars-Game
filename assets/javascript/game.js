9b4fddfea13aec7d0923877e2a5e263b4e8ce79e
Josh Loftis <jshlfts32@gmail.com>
2017-09-02 01:12:52 -0400

    JS logic calculates attack, hp and counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,21 @@
     name: ["Luke Skywalker", "Darth Vader", "Rey Skywalker", "Kylo Ren"],
     attack: [8, 10, 5, 4],
     health: [120, 100, 150, 180],
-    counter: [10, 15, 8, 22]
+    counter: [10, 15, 8, 22],
+    baseAttack: [8, 10, 5, 4]
   };
   var playerChosen = false;
   var enemyChosen = false;
   var gameOver = false;
+  var enemyDefeated = false;
   var chosenPlayer;
+  var playerHP;
+  var playerAttack;
+  var playerBaseAttack;
   var chosenEnemy;
+  var enemyCounter;
+  var enemyHP;
+
 
 
 
@@ -21,13 +29,13 @@
 //-----------------------------------------------------------------------------
 
   $('.charDiv').each(function(i) {
-    $(this).data({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i]})
+    $(this).data({"name" : charAttr.name[i], "attack" : charAttr.attack[i], "health" : charAttr.health[i], "counter" : charAttr.counter[i], "baseAttack" : charAttr.baseAttack[i]})
     .append('<div class="name">' + charAttr.name[i] + "</div>")
     .append('<img class="img-responsive center-block charImg" ' + charAttr.image[i] + '>')
     .append('<div class="health">' + charAttr.health[i] + "</div>");
   });
 
-
+  console.log($('#char1').data());
 
 
 // Game Logic
@@ -37,18 +45,45 @@ $(document).ready(function() {
   $('.charDiv').on('click', function () {
     if (playerChosen == false) {
       chosenPlayer = $(this);
+      playerHP = $(chosenPlayer).data("health");
+      playerAttack = $(chosenPlayer).data("attack");
+      playerBaseAttack = $(chosenPlayer).data("baseAttack");
+      console.log(playerHP);
+      console.log(playerAttack);
       $('#playerArea').append(this);
-      $(this).unbind('click');
+      $(this).off('click');
       $('.charDiv').not(this).appendTo('#holdingBin');
       playerChosen = true;
     } else if (enemyChosen == true){
     } else {
       $('#defenderArea').html(this);
-      $(this).unbind('click');
+      $(this).off('click');
       enemyChosen = true;
       chosenEnemy = $(this);
+      enemyHP = $(chosenEnemy).data("health");
+      enemyCounter = $(chosenEnemy).data("counter");
     }
-  });
+    });
+    $('#attackBtn').on('click', function() {
+      if (enemyDefeated == false) {
+        playerHP -= enemyCounter;
+        console.log("Player HP is " + playerHP);
+        enemyHP -= playerAttack;
+        console.log("Enemy HP is " + enemyHP);
+        playerAttack += playerBaseAttack;
+        console.log("Player attack is " + playerAttack);
+         if (enemyHP <= 0) {
+          enemyDefeated = true;
+          enemyChosen = false;
+          $('#defenderArea').empty();
+        }
+      } else if (enemyDefeated == true) {
+        if (enemyChosen == false) {
+          $('#defenderArea').html("An enemy you most choose.");
+        }
+      } else {
+      }
+    });
 
 
 });
